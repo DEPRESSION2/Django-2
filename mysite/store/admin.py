d99fa0957bf13d27b87ba7a4b695c6717496c0e9
Deprion <86678150+DEPRESSION2@users.noreply.github.com>
2021-12-25 11:40:59 +0200

    Delete mysite directory

--- mysite/store/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from store.models import CustomUser, Product, Purchase, PurchaseReturns
-
-admin.site.register(CustomUser)
-admin.site.register(Product)
-admin.site.register(Purchase)
-admin.site.register(PurchaseReturns)
